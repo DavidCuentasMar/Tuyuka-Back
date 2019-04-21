9201e46c8931a36dc22b64a6fb66310ed79adc62
laptop <laptop@MacBook-Pro-de-laptop.local>
2019-04-21 15:20:58 -0500

    PostgreSQL connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const db = require('./database')
+const bodyParser = require('body-parser')
+
 
 // initialization
 const app = express();
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 // Settings
 app.set("port", process.env.PORT || 3000);
@@ -15,6 +24,9 @@ app.use(express.json());
 app.get('/', (req,res)=>{
   res.send('Hello World');
 });
+app.get('/users', db.getUsers)
+app.post('/newuser', db.createUser)
+
 
 // Starting the server
 const server = app.listen(app.get("port"), () => {
